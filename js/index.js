0b34359286742bcd9af6f06796ca990452280c80
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-16 17:32:24 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-// use this to copy code snippets or use your browser's console
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
 }
- 
-let sally = { name: 'Sally' };
-
-greet.call(sally);
-
-greet.apply(sally);
